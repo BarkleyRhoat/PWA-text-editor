53838a907608ad93d39d5b5c523120e840160f12
BarkleyRhoat <barkleyrhoat@gmail.com>
2024-02-12 20:01:54 -0500

    commented out sections in db.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,7 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// export const putDb = async (content) => console.error('putDb not implemented');
 
  export const putDb = async (content) => {
    const db = await openDB('jate', 1);
@@ -27,7 +27,7 @@ export const putDb = async (content) => console.error('putDb not implemented');
 
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// export const getDb = async () => console.error('getDb not implemented');
 export const getDb = async () => {
   const db = await openDB('jate', 1);
   const transaction = db.transaction('jate', 'readonly');
