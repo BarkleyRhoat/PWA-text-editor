642f4d20d8760c30557c6752c75efa554ae5f87d
BarkleyRhoat <barkleyrhoat@gmail.com>
2024-02-12 19:58:31 -0500

    added an event handler to the before install promt and implemented a click even handler on the butInstall and added a handler for appinstall event

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,9 +3,36 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {});
+async function installPWA(event) {
+  // Prevent the mini-infobar from appearing on mobile
+  event.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = event;
+  // Update UI notify the user they can install the PWA
+  butInstall.style.display = 'block';
+}
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => { 
+  // Show the install prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  const { outcome } = await deferredPrompt.userChoice;
+  // Optionally, send analytics event with outcome of user choice
+  console.log(`User response to the install prompt: ${outcome}`);
+  // We no longer need the prompt. Clear it up.
+  deferredPrompt = null;
+  // Hide the install button
+  butInstall.style.display = 'none';
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+  // Clear the deferredPrompt so it can be garbage collected
+  deferredPrompt = null;
+  // Log the installation to your analytics
+  console.log('Jate PWA was installed', event);
+}
+);
+
+
