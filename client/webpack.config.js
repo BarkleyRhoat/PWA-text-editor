e3f7530716b89092d85ffaf1b59ddef939f32392
BarkleyRhoat <barkleyrhoat@gmail.com>
2024-02-14 20:47:19 -0500

    commented out module.exports

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,48 +1,48 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
-const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './src/index.html',
-        filename: 'index.html',
-        chunks: ['main'],
+        template: "./src/index.html",
+        filename: "index.html",
+        chunks: ["main"],
       }),
       new HtmlWebpackPlugin({
-        template: './src/install.html',
-        filename: 'install.html',
-        chunks: ['install'],
+        template: "./src/install.html",
+        filename: "install.html",
+        chunks: ["install"],
       }),
       new WebpackPwaManifest({
-        name: 'PWA Text Editor',
-        short_name: 'Text Editor',
-        description: 'A progressive web app text editor',
-        background_color: '#ffffff',
-        theme_color: '#000000',
+        name: "PWA Text Editor",
+        short_name: "Text Editor",
+        description: "A progressive web app text editor",
+        background_color: "#ffffff",
+        theme_color: "#000000",
         icons: [
           {
-            src: path.resolve('src/assets/icon.png'),
+            src: path.resolve("src/assets/icon.png"),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
+            destination: path.join("assets", "icons"),
           },
         ],
       }),
       new InjectManifest({
-        swSrc: './src/service-worker.js',
-        swDest: 'service-worker.js',
+        swSrc: "./src/service-worker.js",
+        swDest: "service-worker.js",
       }),
       new WorkboxWebpackPlugin.GenerateSW({
         clientsClaim: true,
@@ -55,49 +55,43 @@ module.exports = () => {
         //  CSS loaders and babel here
         {
           test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
+          use: ["style-loader", "css-loader"],
         },
         {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
-              plugins : ['@babel/plugin-transform-runtime']
+              presets: ["@babel/preset-env"],
+              plugins: ["@babel/plugin-transform-runtime"],
             },
           },
         },
         {
           test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
-        }
-        
+          use: ["style-loader", "css-loader"],
+        },
       ],
     },
   };
 };
 
+// module.exports = () => {
+//   return {
+//     mode: "development",
+//     entry: {
+//       main: "./src/js/index.js",
+//       install: "./src/js/install.js",
+//     },
+//     output: {
+//       filename: "[name].bundle.js",
+//       path: path.resolve(__dirname, "dist"),
+//     },
+//     plugins: [],
 
-module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
-
-    module: {
-      rules: [
-        
-      ],
-    },
-  };
-};
+//     module: {
+//       rules: [],
+//     },
+//   };
+// };
