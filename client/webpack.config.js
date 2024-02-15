f28aec4a33c4e111454db47ca6c1c55d0ed9d6c7
BarkleyRhoat <barkleyrhoat@gmail.com>
2024-02-14 20:50:51 -0500

    fixing manifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -34,7 +34,7 @@ module.exports = () => {
         theme_color: "#000000",
         icons: [
           {
-            src: path.resolve("src/assets/icon.png"),
+            src: path.resolve("src/images/logo.png"),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join("assets", "icons"),
           },
