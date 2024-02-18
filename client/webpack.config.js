a5bbcc2465c17f86cd4cd91909276b02a22722e9
BarkleyRhoat <barkleyrhoat@gmail.com>
2024-02-17 20:38:48 -0500

    changed path for index.html

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = () => {
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: "./src/client/index.html",
+        template: "./index.html",
         filename: "index.html",
         chunks: ["main"],
       }),
