5f83c7c27d7b7f4cb8b9b73a5296f242aeeec628
BarkleyRhoat <barkleyrhoat@gmail.com>
2024-02-27 18:45:12 -0500

    fixing some bugs

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,29 +1,39 @@
+import { request } from 'express';
 import { openDB } from 'idb';
 
 const initdb = async () => {
   const db = await openDB('jate', 1, {
     upgrade(db) {
       if (!db.objectStoreNames.contains('jate')) {
-        const store = db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+        // const store =
+         db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
         // Add any additional logic for upgrading the schema if needed
         console.log('jate database created');
       }
     },
   });
-  return db;
+  // return db;
 };
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 // export const putDb = async (content) => console.error('putDb not implemented');
 
  export const putDb = async (content) => {
+    try{
+
    const db = await openDB('jate', 1);
    const transaction = db.transaction('jate', 'readwrite');
    const store = transaction.objectStore('jate');
-   await store.add(content);
-  await transaction.done;
-  return db;
-};
+   const request = store.put({ id:1, value: content});
+   const result = await request;
+   console.log('dada saved to the database', result);
+  //  await store.add(content);
+  // await transaction.done;
+  // return db;
+    }catch (err) {
+      console.error('no putDb');
+    }
+  };
 
 
 
@@ -31,12 +41,20 @@ const initdb = async () => {
 // TODO: Add logic for a method that gets all the content from the database
 // export const getDb = async () => console.error('getDb not implemented');
 export const getDb = async () => {
+  try{
+
   const db = await openDB('jate', 1);
   const transaction = db.transaction('jate', 'readonly');
   const store = transaction.objectStore('jate');
-  const content = await store.getAll();
-  await transaction.done;
-  return content;
+  const request = await store.getAll();
+  const result = await request;
+
+  return result?.value;
+  // await transaction.done;
+  // return content;
+} catch (err) {
+  console.error('not getDb')
+}
 };
 
 
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,34 +2,35 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
-async function installPWA(event) {
-  // Prevent the mini-infobar from appearing on mobile
-  event.preventDefault();
+window.addEventListener('beforeinstallprompt', (event) => {
   // Stash the event so it can be triggered later.
-  deferredPrompt = event;
-  // Update UI notify the user they can install the PWA
-  butInstall.style.display = 'block';
-}
+  window.deferredPrompt = event;
+  // remove
+  butInstall.classList.toggle('hidden, false');
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => { 
-  // Show the install prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  const { outcome } = await deferredPrompt.userChoice;
-  // Optionally, send analytics event with outcome of user choice
-  console.log(`User response to the install prompt: ${outcome}`);
-  // We no longer need the prompt. Clear it up.
-  deferredPrompt = null;
-  // Hide the install button
-  butInstall.style.display = 'none';
+butInstall.addEventListener('click', async () => {
+
+  const promptEvent = window.deferredPrompt;
+
+  if (!promptEvent) {
+   return;
+  }
+
+  //prompt
+  promptEvent.prompt();
+  
+  // Reset the deferred prompt variable, it can only be used once.
+  window.deferredPrompt = null;
+  
+  butInstall.classList.toggle('hidden', true);
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
   // Clear the deferredPrompt so it can be garbage collected
-  deferredPrompt = null;
+  window.deferredPrompt = null;
   // Log the installation to your analytics
   console.log('Jate PWA was installed', event);
 }
