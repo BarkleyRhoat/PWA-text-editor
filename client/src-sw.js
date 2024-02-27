6e2b3b04bc9104e865c1376643ffc603e0cad76f
BarkleyRhoat <barkleyrhoat@gmail.com>
2024-02-27 18:58:46 -0500

    revert back to original

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,28 +27,13 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-// const assetCache = new CacheFirst({
-//   cacheName: 'asset-cache',
-// });
+const assetCache = new CacheFirst({
+  cacheName: 'asset-cache',
+});
 
-// registerRoute(
-//   ({ request }) => request.destination === 'style' || request.destination === 'script',
-//   assetCache
-// );
-// registerRoute();
 registerRoute(
-
-  // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
-  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-  new StaleWhileRevalidate({
-    // Name of the cache storage.
-    cacheName: 'asset-cache',
-    plugins: [
-      // This plugin will cache responses with these headers to a maximum-age of 30 days
-      new CacheableResponsePlugin({
-        statuses: [0, 200],
-      }),
-    ],
-  })
-
+  ({ request }) => request.destination === 'style' || request.destination === 'script',
+  assetCache
 );
+registerRoute();
+
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,36 +1,72 @@
+// const butInstall = document.getElementById('buttonInstall');
+
+// // Logic for installing the PWA
+// // TODO: Add an event handler to the `beforeinstallprompt` event
+// window.addEventListener('beforeinstallprompt', (event) => {
+//   // Stash the event so it can be triggered later.
+//   window.deferredPrompt = event;
+//   // remove
+//   butInstall.classList.toggle('hidden, false');
+// });
+
+// // TODO: Implement a click event handler on the `butInstall` element
+// butInstall.addEventListener('click', async () => {
+
+//   const promptEvent = window.deferredPrompt;
+
+//   if (!promptEvent) {
+//    return;
+//   }
+
+//   //prompt
+//   promptEvent.prompt();
+  
+//   // Reset the deferred prompt variable, it can only be used once.
+//   window.deferredPrompt = null;
+  
+//   butInstall.classList.toggle('hidden', true);
+// });
+
+// // TODO: Add an handler for the `appinstalled` event
+// window.addEventListener('appinstalled', (event) => {
+//   // Clear the deferredPrompt so it can be garbage collected
+//   window.deferredPrompt = null;
+//   // Log the installation to your analytics
+//   console.log('Jate PWA was installed', event);
+// }
+// );
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {
+window.addEventListener('beforeinstallprompt', (event) => {});
+async function installPWA(event) {
+  // Prevent the mini-infobar from appearing on mobile
+  event.preventDefault();
   // Stash the event so it can be triggered later.
-  window.deferredPrompt = event;
-  // remove
-  butInstall.classList.toggle('hidden, false');
-});
+  deferredPrompt = event;
+  // Update UI notify the user they can install the PWA
+  butInstall.style.display = 'block';
+}
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {
-
-  const promptEvent = window.deferredPrompt;
-
-  if (!promptEvent) {
-   return;
-  }
-
-  //prompt
-  promptEvent.prompt();
-  
-  // Reset the deferred prompt variable, it can only be used once.
-  window.deferredPrompt = null;
-  
-  butInstall.classList.toggle('hidden', true);
+butInstall.addEventListener('click', async () => { 
+  // Show the install prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  const { outcome } = await deferredPrompt.userChoice;
+  // Optionally, send analytics event with outcome of user choice
+  console.log(`User response to the install prompt: ${outcome}`);
+  // We no longer need the prompt. Clear it up.
+  deferredPrompt = null;
+  // Hide the install button
+  butInstall.style.display = 'none';
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
   // Clear the deferredPrompt so it can be garbage collected
-  window.deferredPrompt = null;
+  deferredPrompt = null;
   // Log the installation to your analytics
   console.log('Jate PWA was installed', event);
 }
