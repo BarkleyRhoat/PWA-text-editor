9abcc4763ac39e3d89c2123b6d7e6577ab17abe7
BarkleyRhoat <barkleyrhoat@gmail.com>
2024-02-27 19:28:28 -0500

    changed src-sw.js file for implenting asset caching

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,61 +1,6 @@
 // import { request } from 'express';
 import { openDB } from 'idb';
 
-// const initdb = async () => {
-//   const db = await openDB('jate', 1, {
-//     upgrade(db) {
-//       if (!db.objectStoreNames.contains('jate')) {
-//         // const store =
-//          db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-//         // Add any additional logic for upgrading the schema if needed
-//         console.log('jate database created');
-//       }
-//     },
-//   });
-//   // return db;
-// };
-
-// // TODO: Add logic to a method that accepts some content and adds it to the database
-// // export const putDb = async (content) => console.error('putDb not implemented');
-
-//  export const putDb = async (content) => {
-//     try{
-
-//    const db = await openDB('jate', 1);
-//    const transaction = db.transaction('jate', 'readwrite');
-//    const store = transaction.objectStore('jate');
-//    const request = store.put({ id:1, value: content});
-//    const result = await request;
-//    console.log('dada saved to the database', result);
-//   //  await store.add(content);
-//   // await transaction.done;
-//   // return db;
-//     }catch (err) {
-//       console.error('no putDb');
-//     }
-//   };
-
-
-
-
-// // TODO: Add logic for a method that gets all the content from the database
-// // export const getDb = async () => console.error('getDb not implemented');
-// export const getDb = async () => {
-//   try{
-
-//   const db = await openDB('jate', 1);
-//   const transaction = db.transaction('jate', 'readonly');
-//   const store = transaction.objectStore('jate');
-//   const request = await store.getAll();
-//   const result = await request;
-
-//   return result?.value;
-//   // await transaction.done;
-//   // return content;
-// } catch (err) {
-//   console.error('not getDb')
-// }
-// };
 const initdb = async () => {
   const db = await openDB('jate', 1, {
     upgrade(db) {
@@ -69,7 +14,7 @@ const initdb = async () => {
   return db;
 };
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+//  Add logic to a method that accepts some content and adds it to the database
 // export const putDb = async (content) => console.error('putDb not implemented');
 
  export const putDb = async (content) => {
@@ -83,8 +28,7 @@ const initdb = async () => {
 
 
 
-
-// TODO: Add logic for a method that gets all the content from the database
+ // Add logic for a method that gets all the content from the database
 // export const getDb = async () => console.error('getDb not implemented');
 export const getDb = async () => {
   const db = await openDB('jate', 1);
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,44 +1,7 @@
-// const butInstall = document.getElementById('buttonInstall');
-
-// // Logic for installing the PWA
-// // TODO: Add an event handler to the `beforeinstallprompt` event
-// window.addEventListener('beforeinstallprompt', (event) => {
-//   // Stash the event so it can be triggered later.
-//   window.deferredPrompt = event;
-//   // remove
-//   butInstall.classList.toggle('hidden, false');
-// });
-
-// // TODO: Implement a click event handler on the `butInstall` element
-// butInstall.addEventListener('click', async () => {
-
-//   const promptEvent = window.deferredPrompt;
-
-//   if (!promptEvent) {
-//    return;
-//   }
-
-//   //prompt
-//   promptEvent.prompt();
-  
-//   // Reset the deferred prompt variable, it can only be used once.
-//   window.deferredPrompt = null;
-  
-//   butInstall.classList.toggle('hidden', true);
-// });
-
-// // TODO: Add an handler for the `appinstalled` event
-// window.addEventListener('appinstalled', (event) => {
-//   // Clear the deferredPrompt so it can be garbage collected
-//   window.deferredPrompt = null;
-//   // Log the installation to your analytics
-//   console.log('Jate PWA was installed', event);
-// }
-// );
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
+//  Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {});
 async function installPWA(event) {
   // Prevent the mini-infobar from appearing on mobile
@@ -49,7 +12,7 @@ async function installPWA(event) {
   butInstall.style.display = 'block';
 }
 
-// TODO: Implement a click event handler on the `butInstall` element
+//  Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => { 
   // Show the install prompt
   deferredPrompt.prompt();
@@ -63,7 +26,7 @@ butInstall.addEventListener('click', async () => {
   butInstall.style.display = 'none';
 });
 
-// TODO: Add an handler for the `appinstalled` event
+//  Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
   // Clear the deferredPrompt so it can be garbage collected
   deferredPrompt = null;
